79c3b76cbea62b8ae49166915745d1fbbfe0fb99
unknown <przetacznkp@gmail.com>
2021-08-06 15:44:11 +0200

    initial commit, custom font

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import './pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trvl',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Raleway',
+      ),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
